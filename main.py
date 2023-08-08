db8f7b8ea149248f58e058d77a531f01191df0f4
Jakub Nowocień <57866768+Peggaz@users.noreply.github.com>
2023-08-08 21:50:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class RigidBody:
+    def __init__(self, mass, initial_velocity, initial_angular_velocity):
+        self.mass = mass
+        self.velocity = initial_velocity
+        self.angular_velocity = initial_angular_velocity
+    
+    def change_in_momentum(self, time_interval):
+        return self.mass * self.velocity * time_interval
+    
+    def change_in_angular_momentum(self, time_interval):
+        return self.mass * self.angular_velocity * time_interval
+    
+def visualize_3d(material_name, data):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    time = np.linspace(0, 10, len(data))
+    ax.plot(time, data, range(len(data)))
+    
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Change in Momentum')
+    ax.set_zlabel('Step')
+    ax.set_title(f'Change in Momentum for {material_name}')
+    
+    plt.show()
 
+# Define rigid bodies for steel and aluminum
+steel = RigidBody(mass=10, initial_velocity=5, initial_angular_velocity=2)
+aluminium = RigidBody(mass=8, initial_velocity=7, initial_angular_velocity=3)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Calculate change in momentum over time
+time_interval = 0.1
+num_time_steps = 100
+steel_momentum_changes = [steel.change_in_momentum(time_interval) for _ in range(num_time_steps)]
+aluminium_momentum_changes = [aluminium.change_in_momentum(time_interval) for _ in range(num_time_steps)]
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Visualize the change in momentum in 3D for steel and aluminum
+visualize_3d("Steel", steel_momentum_changes)
+visualize_3d("Aluminium", aluminium_momentum_changes)
